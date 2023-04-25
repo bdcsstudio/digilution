264edab8cff7f2d1e6eaf7335ae7f42758cd590e
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-04-25 19:43:14 +0200

    Create page-transition.js

---
+++ page-transition.js
@@ -0,0 +1,28 @@
+// Works on internal links, ignores external links or links containing #
+function internalLink(myLink) {
+  return myLink.host == window.location.host;
+}
+$("a").each(function () {
+  if (internalLink(this) && this.href.indexOf("#") === -1) {
+    $(this).click(function (e) {
+      e.preventDefault();
+      var moduleURL = jQuery(this).attr("href");
+      var transitionTime = $(this).attr("page-transition-time") || 1000;
+      setTimeout(function () {
+        window.location = moduleURL;
+      }, transitionTime);
+
+      // Class that has page out interaction tied to click
+      $('[page-transition="component"]').click();
+
+      // Trigger custom event for GSAP interaction
+      $(document).trigger("page-transition");
+
+      $(window).bind("pageshow", function (event) {
+        if (event.originalEvent.persisted) {
+          window.location.reload();
+        }
+      });
+    });
+  }
+});
