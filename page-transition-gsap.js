ba36208186807650c2f34b094f6f267397af775e
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-04-25 19:57:26 +0200

    Update page-transition-gsap.js

--- page-transition-gsap.js
+++ page-transition-gsap.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).on('page-transition', function() {
   // GSAP Animation
   gsap.fromTo('[page-transition-bg-radius="target"]', {
-    borderTopLeftRadius: '0%',
-    borderTopRightRadius: '0%',
-  }, {
-    duration: 1,
     borderTopLeftRadius: '100%',
     borderTopRightRadius: '100%',
+  }, {
+    duration: 1,
+    borderTopLeftRadius: '0%',
+    borderTopRightRadius: '0%',
     ease: 'power4.inOut',
-    delay: 1.5
+    delay: 0.2
   });
 });
